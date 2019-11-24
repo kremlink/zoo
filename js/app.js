ce84dcb33585c1816cab4970aacb0cad507bee8f
Sasha <kremlink0@gmail.com>
2019-11-24 23:15:05 +0300

    index finish

--- js/app.js
+++ js/app.js
@@ -6,6 +6,19 @@
  mgr.overrideData();
 
  $(function(){
+  mgr.set({data:'shared.shiftPop',object:{
+    show:function(e,opts){
+     var u=this.get('data').extra;
+
+     opts.pop.css('top',mgr.helpers.win.scrollTop()+u.shift);
+    },
+    hide:function(e,opts){
+     var u=this.get('data').extra;
+
+     opts.pop.css('top',0);
+    }
+   },lib:false});
+
   mgr.trigger('app:ready',[mgr.get('shared.modules')]);
  });
 }));
